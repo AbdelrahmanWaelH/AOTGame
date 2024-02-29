34f3b45151d2d1cfe8c1d6e9f1f646016d36b6d1
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-02-29 21:40:03 +0200

    removed tester main method

--- src/WeaponCSV.java
+++ src/WeaponCSV.java
@@ -10,11 +10,4 @@ public class WeaponCSV extends CSVUser {
 	public void writeCSV(String[] lines){
 		super.writeCSV(lines, weaponFile);
 	}
-	public static void main (String args[]){
-			WeaponCSV w = new WeaponCSV();
-			w.readCSV();
-			
-			TitanCSV t = new TitanCSV();
-			t.readCSV();
-	}
 }

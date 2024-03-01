de58615b0950079daa44a75704c14c302c7953b3
Magdy2004 <omarmagdy11964@gmail.com>
2024-03-01 19:27:25 +0200

    .
    .

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -1,15 +1,12 @@
 package game.engine.weapons.factory;
-<<<<<<< HEAD
 import java.io.*;
 import game.engine.dataloader.*;
 import game.engine.weapons.WeaponRegistry;
 import java.util.*;
-=======
 import java.io.IOException;
 import game.engine.weapons.WeaponRegistry;
 import java.util.HashMap;
 import game.engine.dataloader.DataLoader;
->>>>>>> 1f78ae1a38c25af06f9dcc7b1efb78407e85cada
 
 public class WeaponFactory {
 	private HashMap<Integer, WeaponRegistry> weaponShop;

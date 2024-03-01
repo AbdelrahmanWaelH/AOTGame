984b01874bbbd3c8bd73bcfea4de995acaee4b0c
Magdy2004 <omarmagdy11964@gmail.com>
2024-03-01 19:25:47 +0200

    .
    .

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -1,18 +1,15 @@
 package game.engine.weapons.factory;
 import java.io.*;
-
-import game.engine.weapons.WeaponRegistry;
-
-import java.util.*;
-
 import game.engine.dataloader.*;
+import game.engine.weapons.WeaponRegistry;
+import java.util.*;
 
 public class WeaponFactory {
 	private HashMap<Integer, WeaponRegistry> weaponShop;
 	File csvFile= new File("weapons.csv");
 	
 	public WeaponFactory() throws IOException{ 
-     this.weaponShop = DataLoader.readWeaponRegistry();
+        this.weaponShop=DataLoader.readWeaponRegistry();
 	}
 	
 	public HashMap<Integer, WeaponRegistry> getWeaponShop() {

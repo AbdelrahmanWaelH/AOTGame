4fe97420b09cb2997796f7a89d209b419723a1d3
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-03-01 19:04:42 +0200

    integrated dataloader and weaponfactory

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -1,33 +1,18 @@
 package game.engine.weapons.factory;
-import java.io.File;
-import java.io.BufferedReader; 
-import java.io.FileReader;
-import java.io.IOException; 
+import java.io.*;
 
 import game.engine.weapons.WeaponRegistry;
 
 import java.util.*;
 
+import game.engine.dataloader.*;
+
 public class WeaponFactory {
 	private HashMap<Integer, WeaponRegistry> weaponShop;
-	File csvFile= new File("C:\\Users\\Omar Magdy\\OneDrive\\Documents\\GitHub\\AOTGame\\src");
+	File csvFile= new File("weapons.csv");
 	
 	public WeaponFactory() throws IOException{ 
-        try {
-        	@SuppressWarnings("resource")
-			BufferedReader currLine = new BufferedReader(new FileReader(csvFile));
-            String line=currLine.readLine();
-            while (line != null) {
-                Object[] parts = line.split(",");
-                if (parts.length >= 2) {
-                    Integer code = (Integer) parts[0];
-                    WeaponRegistry weapon = (WeaponRegistry) parts[1];
-                    this.weaponShop.put(code, weapon);
-                }
-                line=currLine.readLine();
-            }
-        }finally{
-        }
+     this.weaponShop = DataLoader.readWeaponRegistry();
 	}
 	
 	public HashMap<Integer, WeaponRegistry> getWeaponShop() {

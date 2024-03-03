fbd70ff8a6fcbcb9f2036ea6346caf11241a90dc
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-03-03 21:25:13 +0200

    Revert "Added IntelliJ support"
    This reverts commit 96e2ee030dfd41dfa1fb5a406d9fe6e301dfb391.

--- src/game/engine/dataloader/DataLoader.java
+++ src/game/engine/dataloader/DataLoader.java
@@ -56,19 +56,19 @@ public class DataLoader {
 	
 	
 	// this was just used to test the static methods written above 
-		public static void main(String[] args) {
-	        try {
-	            HashMap<Integer, WeaponRegistry> weaponRegistryMap = readWeaponRegistry();
-	            printWeaponRegistry(weaponRegistryMap);
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-
-	    public static void printWeaponRegistry(HashMap<Integer, WeaponRegistry> weaponRegistryMap) {
-	        for (Map.Entry<Integer, WeaponRegistry> entry : weaponRegistryMap.entrySet()) {
-	            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
-	        }
-	    }
+//		public static void main(String[] args) {
+//	        try {
+//	            HashMap<Integer, WeaponRegistry> weaponRegistryMap = readWeaponRegistry();
+//	            printWeaponRegistry(weaponRegistryMap);
+//	        } catch (IOException e) {
+//	            e.printStackTrace();
+//	        }
+//	    }
+//
+//	    public static void printWeaponRegistry(HashMap<Integer, WeaponRegistry> weaponRegistryMap) {
+//	        for (Map.Entry<Integer, WeaponRegistry> entry : weaponRegistryMap.entrySet()) {
+//	            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//	        }
+//	    }
 	
 }

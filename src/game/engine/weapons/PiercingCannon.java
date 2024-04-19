9366a491299e0798f2957c1350508dcf1399a353
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-04-19 17:50:15 +0200

    updated piercing cannon attack method

--- src/game/engine/weapons/PiercingCannon.java
+++ src/game/engine/weapons/PiercingCannon.java
@@ -15,18 +15,20 @@ public class PiercingCannon extends Weapon
 
 	@Override
 	public int turnAttack(PriorityQueue<Titan> laneTitans) {
-		int resourcesGained = 0;
+		int totalResourcesGained = 0;
 		PriorityQueue<Titan> removedTitans = new PriorityQueue<>();
 		for (int i = 0; i < 5 && !laneTitans.isEmpty(); i++){
 			Titan t = laneTitans.remove();
-			resourcesGained	+= t.takeDamage(this.getDamage());
-			removedTitans.add(t);
+			int resourcesGained	= t.takeDamage(this.getDamage());
+			if (resourcesGained == 0){
+			removedTitans.add(t);} //titan will be re-added if it is not defeated
+			totalResourcesGained += resourcesGained;
 		} //pop 5 titans if available, apply damage to them
 		while (!removedTitans.isEmpty()){
 			Titan t = removedTitans.remove();
 			laneTitans.add(t);
 		} //add popped titans back into the original priority queue, their order will be figured out automatically by the PQ ADT
-		return resourcesGained;
+		return totalResourcesGained;
 	}
 
 }

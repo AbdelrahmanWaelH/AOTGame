f6108cbda8792169d3407257193f14536283e88e
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-04-20 16:13:42 +0200

    minor changes to weaponfactory

--- src/game/engine/weapons/factory/WeaponFactory.java
+++ src/game/engine/weapons/factory/WeaponFactory.java
@@ -23,25 +23,24 @@ public class WeaponFactory
 		return weaponShop;
 	}
 	public FactoryResponse buyWeapon(int resources, int weaponCode) throws InsufficientResourcesException{
-		WeaponRegistry y= weaponShop.get(weaponCode);
-		if(resources>=y.getPrice()){
-			return new FactoryResponse(y.buildWeapon(),resources-y.getPrice());
+		WeaponRegistry reg = weaponShop.get(weaponCode);
+		if(resources>= reg.getPrice()){
+			return new FactoryResponse(reg.buildWeapon(),resources-reg.getPrice());
 		}
 		else
 			throw new InsufficientResourcesException(resources);
-		
 	}
 	public void addWeaponToShop(int code, int price){
-		WeaponRegistry y= new WeaponRegistry(code,price);
-		weaponShop.put(code,y);
+		WeaponRegistry reg = new WeaponRegistry(code,price);
+		weaponShop.put(code,reg);
 	}
 	public void addWeaponToShop(int code, int price, int damage, String name){
-		WeaponRegistry y= new WeaponRegistry(code,price,damage,name);
-		weaponShop.put(code,y);
+		WeaponRegistry reg = new WeaponRegistry(code,price,damage,name);
+		weaponShop.put(code,reg);
 	}
 	public void addWeaponToShop(int code, int price, int damage, String name, int minRange,int maxRange){
-		WeaponRegistry y= new WeaponRegistry(code,price,damage,name,minRange,maxRange);
-		weaponShop.put(code,y);
+		WeaponRegistry reg = new WeaponRegistry(code,price,damage,name,minRange,maxRange);
+		weaponShop.put(code,reg);
 	}
 
 }

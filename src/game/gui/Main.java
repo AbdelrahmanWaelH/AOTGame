af5697a2c4868e9c205f691972d88b572a512434
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-04-25 13:43:57 +0200

    Hello World GUI!!!

--- src/game/gui/Main.java
+++ src/game/gui/Main.java
@@ -3,6 +3,7 @@ package game.gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 
 
@@ -15,6 +16,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			Label l1 = new Label("Hello World");
+			root.setCenter(l1);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}

2d546a17bf30d46b6b640682e9b74262b666fb51
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-05-02 00:52:15 +0300

    Max, fullscreen, title bar & icon. Instance of battle suits difficulty

--- src/game/gui/Controller.java
+++ src/game/gui/Controller.java
@@ -1,11 +1,26 @@
 package game.gui;
+import java.io.IOException;
+import game.engine.Battle;
+import javafx.fxml.FXML;
 
 public class Controller {
+	@FXML
+	
 	public void easy(){
-		System.out.println("You have chosen easy mode");
+		try {
+			Battle battle = new Battle(1,0,10,3,250);
+			System.out.println("You have chosen easy mode, instance created");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void hard(){
-		System.out.println("You have chosen hard mode");
+		try {
+			Battle battle = new Battle(1,0,10,5,125);
+			System.out.println("You have chosen hard mode, instance created");
+		} catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 }
--- src/game/gui/Main.java
+++ src/game/gui/Main.java
@@ -3,6 +3,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +11,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try{
+		primaryStage.setTitle("AOT Game pre-alpha");
+		primaryStage.setMaximized(true);
+		primaryStage.getIcons().add(new Image("game/gui/assets/icon.png"));
+		//primaryStage.setFullScreen(true);
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);

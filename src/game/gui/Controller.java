5e72f58cd8893eaa671f70357b4221a817d2f33f
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-05-02 00:19:57 +0300

    easy mode hard mode

---
+++ src/game/gui/Controller.java
@@ -0,0 +1,11 @@
+package game.gui;
+
+public class Controller {
+	public void easy(){
+		System.out.println("You have chosen easy mode");
+	}
+	
+	public void hard(){
+		System.out.println("You have chosen hard mode");
+	}
+}

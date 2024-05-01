88f9a2436c7d399773d35d20f2f5d0556ab77a31
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-05-02 01:14:24 +0300

    splash screen

--- src/game/gui/Main.java
+++ src/game/gui/Main.java
@@ -10,15 +10,29 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		String title = "AOT Game pre-alpha";
+		String iconPath = "game/gui/assets/icon.png";
 		try{
-		primaryStage.setTitle("AOT Game pre-alpha");
-		primaryStage.setMaximized(true);
-		primaryStage.getIcons().add(new Image("game/gui/assets/icon.png"));
-		//primaryStage.setFullScreen(true);
-		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-		Scene scene = new Scene(root);
+		Stage splashStage = new Stage();
+		splashStage.setTitle(title);
+		splashStage.getIcons().add(new Image(iconPath));
+		Parent splashRoot = FXMLLoader.load(getClass().getResource("Splash.fxml"));
+		Scene splashScene = new Scene(splashRoot);
+		splashStage.setScene(splashScene);
+		splashStage.show();
+		splashStage.setMaximized(true);
+		Thread.sleep(3000);
+		splashStage.close();
+
+		
+		primaryStage.setTitle(title);
+		primaryStage.getIcons().add(new Image(iconPath));
+		Parent mainUI = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(mainUI);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		primaryStage.setMaximized(true);
+		//primaryStage.setFullScreen(true);
 		} catch (Exception e){
 			e.printStackTrace();
 		}
